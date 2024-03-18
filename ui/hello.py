9a90da66b98f2281165afd96195c6f2330dc1093
Mi-racle <872137422@qq.com>
2024-03-19 03:52:40 +0800

    add ui

--- ui/hello.py
+++ ui/hello.py
@@ -1,12 +1,13 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QTableWidget, QPushButton, QWidget, QTableWidgetItem, QFileDialog, QProgressBar
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QTableWidget, QPushButton, QWidget, \
+    QTableWidgetItem, QFileDialog, QProgressBar, QHBoxLayout
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("表格示例")
+        self.setWindowTitle("锁销分类联邦学习网络")
         self.setGeometry(100, 100, 800, 600)
 
         self.central_widget = QWidget()
@@ -16,7 +17,7 @@ class MainWindow(QMainWindow):
 
         self.table = QTableWidget()
         self.table.setColumnCount(5)
-        self.table.setHorizontalHeaderLabels(["节点名", "目标服务器IP", "数据集", "控制", "训练进度"])
+        self.table.setHorizontalHeaderLabels(["节点名", "目标服务器IP", "数据集", "操作", "训练进度"])
 
         self.add_row_button = QPushButton("添加节点")
         self.add_row_button.clicked.connect(self.add_row)
@@ -31,33 +32,48 @@ class MainWindow(QMainWindow):
         self.table.insertRow(row_position)
 
         # 添加字符串项
-        string_item = QTableWidgetItem("节点名")
+        string_item = QTableWidgetItem('节点名')
         self.table.setItem(row_position, 0, string_item)
 
         # 添加IP地址项
-        ip_item = QTableWidgetItem("127.0.0.1")
+        ip_item = QTableWidgetItem('127.0.0.1')
         self.table.setItem(row_position, 1, ip_item)
 
-        # 添加选择文件按钮
-        select_file_button = QPushButton("选择文件夹")
-        select_file_button.clicked.connect(lambda _, row=row_position: self.select_file(row))
-        self.table.setCellWidget(row_position, 2, select_file_button)
+        # 添加IP地址项
+        folder_item = QTableWidgetItem('尚未选择')
+        folder_item.setSizeHint(folder_item.sizeHint())
+        self.table.setItem(row_position, 2, folder_item)
 
-        # 添加按钮
-        button = QPushButton("开始")
-        button.clicked.connect(lambda _, btn=button: self.toggle_button(btn))
-        self.table.setCellWidget(row_position, 3, button)
+        # 创建一个QWidget，包含两个按钮
+        button_widget = QWidget()
+        layout = QHBoxLayout()
+
+        # 添加选择文件夹按钮
+        select_folder_button = QPushButton("选择文件夹")
+        select_folder_button.clicked.connect(lambda _, row=row_position: self.select_folder(row))
+        layout.addWidget(select_folder_button)
+
+        # 添加开始/结束按钮
+        start_stop_button = QPushButton("开始")
+        start_stop_button.clicked.connect(lambda _, btn=start_stop_button: self.toggle_button(btn))
+        layout.addWidget(start_stop_button)
+
+        button_widget.setLayout(layout)
+
+        # 将QWidget设置为表格的单元格
+        self.table.setCellWidget(row_position, 3, button_widget)
 
         # 添加进度条
         progress_bar = QProgressBar()
         self.table.setCellWidget(row_position, 4, progress_bar)
 
-    def select_file(self, row):
-        file_dialog = QFileDialog()
-        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
-        if file_dialog.exec():
-            file_path = file_dialog.selectedFiles()[0]
-            self.table.item(row, 2).setText(file_path)
+    def select_folder(self, row):
+        folder_dialog = QFileDialog()
+        folder_dialog.setFileMode(QFileDialog.FileMode.Directory)
+        if folder_dialog.exec():
+            folder_path = folder_dialog.selectedFiles()[0]
+            folder_item = QTableWidgetItem(folder_path)
+            self.table.setItem(row, 2, folder_item)
 
     def toggle_button(self, button):
         if button.text() == "开始":
